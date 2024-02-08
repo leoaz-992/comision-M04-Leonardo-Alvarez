79dcf564e205b704f51f835c98406556f2d48015
Leonardo Alvarez <leo92alvarez25@gmail.com>
2024-02-08 12:21:18 -0300

    ax

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://viaja-app.onrender.com/";
+const API_URL = "https://viaja-app.onrender.com";
 
 const instance = axios.create({
   baseURL: API_URL,
